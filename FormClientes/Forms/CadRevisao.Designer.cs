5c58b592cd3dacc9b47dc9695e499a7aaba87d2c
Pablo de Oliveira Ferreira <pabloliverfe@gmail.com>
2021-02-11 17:33:02 -0300

    correcao consultase implementacao botao pesquisar

--- FormClientes/Forms/CadRevisao.Designer.cs
+++ FormClientes/Forms/CadRevisao.Designer.cs
@@ -52,6 +52,7 @@ namespace FormClientes.Forms
             // 
             // textIdRevisao
             // 
+            this.textIdRevisao.Enabled = false;
             this.textIdRevisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textIdRevisao.Location = new System.Drawing.Point(109, 159);
             this.textIdRevisao.Name = "textIdRevisao";
--- FormClientes/Forms/ConsutaServico.cs
+++ FormClientes/Forms/ConsutaServico.cs
@@ -31,26 +31,69 @@ namespace FormClientes.Forms
             NpgsqlCommand comm = new NpgsqlCommand();
             comm.Connection = conn;
             comm.CommandType = CommandType.Text;
-            comm.CommandText = "select"+'"'+"IdCliente"+'"'+" FROM cliente Where nome="+"'"+nome+"'";
+            comm.CommandText = "select" + '"' + "IdCliente" + '"' + " FROM cliente Where nome Like" + "'%" + nome + "%'";
             NpgsqlDataReader nc = comm.ExecuteReader();
 
+            
             DataTable clientes = new DataTable();
             clientes.Load(nc);
-           
-            dataGridViewConsulta.DataSource = clientes;
-
-
-            MessageBox.Show(comm.CommandText);
-            DataGridViewRow grv = dataGridViewConsulta.Rows[0];
             
-             string idrow = grv.Cells[0].Value.ToString();
-             int numberGrid = int.Parse(idrow);
+            
+           
+            MessageBox.Show("");
+
+            dataGridViewConsulta.DataSource = clientes;
+            
+            MessageBox.Show(comm.CommandText);
+           
+            
+            
+
+            string idrow = dataGridViewConsulta.Rows[0].Cells[0].Value.ToString();
+            int numberGrid = int.Parse(idrow);
 
             MessageBox.Show(idrow);
             comm = new NpgsqlCommand();
             comm.Connection = conn;
             comm.CommandType = CommandType.Text;
             comm.CommandText = "select id_cliente,id_servicos,valtotal from servicos where" + '"' + "id_cliente" + '"' + "=" + numberGrid;
+            MessageBox.Show(comm.CommandText);
+            NpgsqlDataReader dr = comm.ExecuteReader();
+            
+            if (dr.HasRows)
+            {
+
+
+                DataTable dt = new DataTable();
+                dt.Load(dr);
+                dataGridViewConsulta2.DataSource = dt;
+
+
+            }
+            else
+            {
+                MessageBox.Show("Ainda nao exite serviço para esse cliente!","ATENÇÃO!",MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Exclamation);
+            }
+
+            conn.Close();
+
+
+
+
+
+        }
+
+        public void consultaData(string dataDoServico)
+        {
+            DateTime converData = DateTime.Parse(dataDoServico);
+            NpgsqlConnection conn = new NpgsqlConnection("server=localhost;Port=5432;user id=admin; password=admin123;database=ManutInfor");
+
+            conn.Open();
+            
+            NpgsqlCommand comm = new NpgsqlCommand();
+            comm.Connection = conn;
+            comm.CommandType = CommandType.Text;
+            comm.CommandText = "select id_cliente,id_servicos,valtotal from servicos where datadoservico  Like"  +"'%"+converData.ToString("dd/MM/yyyy")+"%'";
             MessageBox.Show(comm.CommandText);
             NpgsqlDataReader dr = comm.ExecuteReader();
 
@@ -66,10 +109,6 @@ namespace FormClientes.Forms
             }
 
             conn.Close();
-
-
-
-
 
         }
         private void CadRevisao_Load(object sender, EventArgs e)
@@ -93,43 +132,28 @@ namespace FormClientes.Forms
 
         private void textNomeCliente_TextChanged(object sender, EventArgs e)
         {
+            
 
-          
         }
 
         private void btnPesquisar_Click(object sender, EventArgs e)
         {
-            if(textNomeCliente.Text!=null && textDataServico.Text==null)
+            if (textNomeCliente.Text != null && textNomeCliente.Text.Length > 2)
             {
                 ConsultaCliente(textNomeCliente.Text);
+
+            }
+
+            else if (textDataServico.Text != null && textDataServico.Text.Length > 2)
+            {
+                consultaData(textDataServico.Text);
+
             }
             else
             {
-                NpgsqlConnection conn = new NpgsqlConnection("server=localhost;Port=5432;user id=admin; password=admin123;database=ManutInfor");
-
-                conn.Open();
-                DateTime dataservico = DateTime.Parse(textDataServico.Text);
-                NpgsqlCommand comm = new NpgsqlCommand();
-                comm.Connection = conn;
-                comm.CommandType = CommandType.Text;
-                comm.CommandText = "select id_cliente,id_servicos,valtotal from servicos where" + '"' + "datadoservico" + '"' + "=" + "'"+ dataservico.ToString("dd/MM/yyyy")+ "'";
-                MessageBox.Show(comm.CommandText);
-                NpgsqlDataReader dr = comm.ExecuteReader();
-
-                if (dr.HasRows)
-                {
-
-
-                    DataTable dt = new DataTable();
-                    dt.Load(dr);
-                    dataGridViewConsulta2.DataSource = dt;
-
-
-                }
-
-                conn.Close();
-
+                MessageBox.Show("Error!","", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
             }
+
         }
     }
 }
